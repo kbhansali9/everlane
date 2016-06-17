6cf3449a869aa273151c4c924811b177f014b374
Kushal Bhansali <kbhanasli9@gmail.com>
2016-06-17 12:16:43 -0400

    bootstrap

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express()
 
 app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
