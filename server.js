1f47c9a9a42c2140ef8f2b2d74a4c15e5e238991
Kushal Bhansali <kbhanasli9@gmail.com>
2016-06-17 12:09:12 -0400

    port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express()
 
+app.set('port', (process.env.PORT || 3000));
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,4 +17,7 @@ app.get('/inventory', InventoryController.all);
 app.get('/checkout', CheckoutController.showCart);
 app.get('/confirm', ChargeController.charge);
 app.use("/style",express.static(__dirname + "/style"));
-app.listen(3000);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
